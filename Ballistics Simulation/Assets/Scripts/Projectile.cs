fcd9e7bc3220ef0ebc9ad30ee0e0b55c12bee10e
Namik Guseinoff <94350345+namikguseinoff@users.noreply.github.com>
2023-09-14 16:41:44 +0400

    Primitive Drag force was added

--- Ballistics Simulation/Assets/Scripts/Projectile.cs
+++ Ballistics Simulation/Assets/Scripts/Projectile.cs
@@ -8,16 +8,25 @@ public class Projectile : MonoBehaviour
 
     private Rigidbody rb;
     private float angle = 0;
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         BallisticSettings = GetComponent<BallisticSettings>();
         Destroy(gameObject, ProjectileProperties.liveTime);
+        Vector3 velocityDirection = transform.up;
+        rb.velocity = ProjectileProperties.StartingSpeed * velocityDirection;
     }
 
     private void FixedUpdate()
     {
         DrawBaseVectors();
+        CalculateDrag();
+    }
+
+    private float GetSpeed()
+    {
+        return rb.velocity.magnitude;
     }
 
     private float GetHeight()
@@ -27,6 +36,12 @@ public class Projectile : MonoBehaviour
 
     private float CalculateDrag()
     {
+        Vector3 dragDirection = -transform.up;
+        Vector3 drag = dragDirection * ProjectileProperties.Drag * BallisticSettings.AtmosphereDensity 
+                        * Mathf.Pow(GetSpeed(), 2) * ProjectileProperties.Area * Mathf.Sin(angle * Mathf.PI / 180);
+
+        rb.AddForce(drag);
+        Debug.Log(drag);
         return 0f;
     }
 
