35ea8a601fe47504609373d88df9f480eae21ee0
Namig Huseynov <94350345+namighuseynov@users.noreply.github.com>
2023-09-19 16:44:25 +0400

    Added script for draw force vectors

--- Ballistics Simulation/Assets/Scripts/Projectile.cs
+++ Ballistics Simulation/Assets/Scripts/Projectile.cs
@@ -6,81 +6,73 @@ public class Projectile : MonoBehaviour
     public ProjectileProperties     ProjectileProperties;
     public BallisticSettings        BallisticSettings;
 
-    public bool                     ShowForceVectors = true;
     private Rigidbody               body;
     public Transform CenterOfMass;
-    private float                   angle = 0;
-    
+
+    public Vector3 Gravity = Vector3.zero;
+    public Vector3 Drag = Vector3.zero;
+    public Vector3 Wind = Vector3.zero;
+
+    //Projectile properies
+    private float area;
+    private float weight;
+    private float dragCoefficent;
+    private float startingSpeed;
+
+    //Ballistic settings
+    private bool useGravity;
+    private bool useDrag;
+    private bool useWind;
+    private float atmoshereDensity;
+
+
     private void Start()
     {
         body = GetComponent<Rigidbody>();
-        BallisticSettings = GameObject.Find("Weapon").GetComponent<BallisticSettings>();
-        Destroy(gameObject, ProjectileProperties.liveTime);
-        Vector3 velocityDirection = transform.up;
-        body.velocity = ProjectileProperties.StartingSpeed * velocityDirection;
+        if (body != null && ProjectileProperties != null)
+        {
+            BallisticSettings = GameObject.Find("Weapon").GetComponent<BallisticSettings>();
+            Destroy(gameObject, ProjectileProperties.liveTime);
+
+            area = ProjectileProperties.Area;
+            weight = ProjectileProperties.Weight;
+            dragCoefficent = ProjectileProperties.dragCoefficient;
+            startingSpeed = ProjectileProperties.StartingSpeed;
+            atmoshereDensity = BallisticSettings.AtmosphereDensity;
+            useGravity = BallisticSettings.useGravity;
+            useDrag = BallisticSettings.useDrag;
+            useWind = BallisticSettings.useWindForce;
+
+            Vector3 velocityDirection = transform.up;
+            body.velocity = startingSpeed * velocityDirection;
+        }
     }
 
     private void FixedUpdate()
     {
         body.useGravity = false;
-        DrawBaseVectors();
 
-        if (ShowForceVectors) { DrawMoveVector(); }
-        if (BallisticSettings.useGravity) { CalculateGravity(); }
-        if (BallisticSettings.useDrag) { CalculateDrag(); }
+        if (useGravity) { CalculateGravity(); }
+        if (useDrag) { CalculateDrag(); }
     }
 
-    private float GetSpeed()
+    private void CalculateGravity()
     {
-        return body.velocity.magnitude;
-    }
-
-    private float GetHeight()
-    {
-        return transform.position.y;
+        Gravity = Physics.gravity * weight;
+        body.AddForceAtPosition(Gravity, CenterOfMass.position);
     }
 
     private void CalculateDrag()
     {
         Vector3 dragDirection = -body.velocity.normalized;
-        Vector3 drag = dragDirection * ProjectileProperties.dragCoefficient * BallisticSettings.AtmosphereDensity 
-                        * Mathf.Pow(GetSpeed(), 2) * ProjectileProperties.Area;
-        body.AddForce(drag);
-        if (ShowForceVectors) { Debug.DrawLine(transform.position, dragDirection + transform.position, Color.gray); }
+        Drag = dragDirection * dragCoefficent * atmoshereDensity
+                        * Mathf.Pow(GetSpeed(), 2) * area;
+        body.AddForce(Drag);
+        Debug.Log(Drag);
     }
 
-    private void CalculateWind()
+    private float GetSpeed()
     {
-        return;
-    }
-
-    private void CalculateGravity()
-    {
-        Vector3 gravity = Physics.gravity*ProjectileProperties.Weight;
-        body.AddForceAtPosition(gravity, CenterOfMass.position);
-        if (ShowForceVectors) { Debug.DrawLine(CenterOfMass.position, CenterOfMass.position + gravity, Color.red); }
-    }
-
-    private void DrawBaseVectors()
-    {
-        Vector3 Axis = transform.position;
-        Vector3 xBasis = transform.up;
-        Vector3 yBasis = transform.up;
-        yBasis.y = 0f;
-        yBasis.Normalize();
-        Debug.DrawLine(Axis, Axis + xBasis, Color.green);
-        Debug.DrawLine(Axis, Axis + yBasis, Color.blue);
-        angle = Vector3.Angle(xBasis, yBasis);
-    }
-    private void DrawMoveVector()
-    {
-        Vector3 moveDirection = transform.position + body.velocity;
-        Debug.DrawLine(transform.position, moveDirection, Color.black);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(CenterOfMass.position, 0.03f);
+        return body.velocity.magnitude;
     }
 }
